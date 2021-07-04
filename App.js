0fd99d4b10ce64c18eef331d3fa0658fde79ca22
Adeola Bamigboye <clazikar@gmail.com>
2021-07-04 02:00:23 +0200

    formspree added

--- App.js
+++ App.js
@@ -11,3 +11,33 @@ window.onscroll = () => {
         myNav.classList.remove("nav-colored");
     }
 };
+
+//FORM SUBMIT
+
+const form = document.getElementById("my-form");
+var statuss = document.getElementById("success-message");
+
+statuss.style.display = 'block'
+
+console.log(status)
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("success-message");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    status.innerHTML = "Thanks for your submission!";
+    status.style.display = 'block';
+    form.reset()
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+    status.style.display = 'block';
+  });
+}
+form.addEventListener("submit", handleSubmit)
